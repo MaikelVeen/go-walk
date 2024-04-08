e2495bbd812e255173142da031ec40053f98d8e7
Maikel Veen <veenmaikel@gmail.com>
2024-04-08 23:06:27 +0200

    Add function processGPXFile function

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"time"
 
+	"github.com/MaikelVeen/go-walk/gpx"
 	"github.com/lmittmann/tint"
 )
 
@@ -17,4 +20,47 @@ func main() {
 	)
 
 	logger.Info("Hello World!")
+
+	walkData, err := processGPXFiles()
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	// TODO: Create function to get all points of a GPX.
+	for _, dat := range walkData {
+		for _, track := range dat.Tracks {
+			for _, segment := range track.Segments {
+				for _, point := range segment.Points {
+					logger.Info("coords", "lat", point.Latitude, "lng", point.Longitude)
+				}
+			}
+		}
+	}
+}
+
+// TODO: Make the folder an argument.
+func processGPXFiles() ([]*gpx.GPX, error) {
+	files, err := os.ReadDir("data")
+	if err != nil {
+		return nil, err
+	}
+
+	var gpxArray []*gpx.GPX
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".gpx" {
+			gpxFile, err := os.Open(fmt.Sprintf("./data/%s", file.Name()))
+			if err != nil {
+				return nil, err
+			}
+
+			g, err := gpx.UnmarshalGPX(gpxFile)
+			if err != nil {
+				return nil, err
+			}
+
+			gpxArray = append(gpxArray, g)
+		}
+	}
+
+	return gpxArray, nil
 }
